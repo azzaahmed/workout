a8ef84c7f99095f5e76a509eba674308541ba9b7
azza ahmed <aazza_ahmed@hotmail.com>
2017-08-10 13:30:00 +0200

    Write creating exercise with invalid inputs feature

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,12 @@
 class Exercise < ApplicationRecord
   belongs_to :user
+
+
+# to rename the attributes and use the new names in validations
+  alias_attribute :workout_details, :workout
+  alias_attribute :activity_date, :workout_date
+  
+  validates :duration_in_min, numericality: { greater_than: 0.0 }
+  validates :workout_details, presence: true
+  validates :activity_date, presence: true
 end
