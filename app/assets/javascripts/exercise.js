c9954cd30a34c5c930f7c2a6fedc275942e092b0
azza ahmed <aazza_ahmed@hotmail.com>
2017-08-10 14:20:36 +0200

    jQuery date picker

---
+++ app/assets/javascripts/exercise.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	// element of id #exercise_workout_date so when click on workout_date field in the form the picker would be displayed
+  $('#exercise_workout_date').datepicker({ dateFormat: 'yy-mm-dd' });
+});
